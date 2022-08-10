6708846941360a49b949826a02959ffb84fff398
cschweda <cschweda@gmail.com>
2022-08-10 11:38:20 -0500

    fix: update DoIT siteimprove URL

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,16 @@
 "use strict";
 
+/* eslint-disable semi */
+
+/* eslint-disable quotes */
+
 /* <![CDATA[ */
 (function () {
-  var sz = document.createElement('script');
-  sz.type = 'text/javascript';
+  var sz = document.createElement("script");
+  sz.type = "text/javascript";
   sz.async = true;
-  sz.src = 'https://siteimproveanalytics.com/js/siteanalyze_6123065.js';
-  var s = document.getElementsByTagName('script')[0];
+  sz.src = "https://siteimproveanalytics.com/js/siteanalyze_6123065.js";
+  var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(sz, s);
 })();
 /* ]]> */
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+/* eslint-disable semi */
+/* eslint-disable quotes */
 /* <![CDATA[ */
 
 (function () {
-  var sz = document.createElement('script'); sz.type = 'text/javascript'; sz.async = true
+  var sz = document.createElement("script");
+  sz.type = "text/javascript";
+  sz.async = true;
 
-  sz.src = 'https://siteimproveanalytics.com/js/siteanalyze_6123065.js'
+  sz.src = "https://siteimproveanalytics.com/js/siteanalyze_6123065.js";
 
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sz, s)
-})()
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(sz, s);
+})();
 
 /* ]]> */
